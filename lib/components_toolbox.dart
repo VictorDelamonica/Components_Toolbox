a6c1e2ac66506de2809c8f757e80b94dcc5592d3
delamonicavictor <victor.delamonica@epitech.eu>
2024-08-12 18:47:09 +0200

    Add Buttons

--- lib/components_toolbox.dart
+++ lib/components_toolbox.dart
@@ -2,7 +2,8 @@ library components_toolbox;
 
 /// region Components
 export './components/auto_text.dart' show AutoText;
-export './components/rounded_buttons.dart' show RoundedButton;
+export './components/rounded_buttons.dart'
+    show RoundedButton, RoundedButtonWithIcons, CircleButton;
 export './components/rounded_container.dart' show RoundedContainer;
 export './components/text_field.dart' show CustomTextField;
 
